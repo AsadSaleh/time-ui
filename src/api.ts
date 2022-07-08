fa8ff44394829055155db30a19ac35f1a06bcdc2
As'ad Saleh Umar <azzzad.s@gmail.com>
2022-07-08 21:12:30 +0700

    Move model and API call to separate file

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+import { TimezoneTime } from "./model";
+
+interface ApiConfig {
+  controller?: AbortController;
+}
+
+export async function getTimezones(): Promise<String[]> {
+  try {
+    const res = await fetch("https://worldtimeapi.org/api").then((r) => {
+      if (!r.ok) {
+        throw new Error(r.statusText);
+      }
+      return r.json() as Promise<String[]>;
+    });
+    console.log({ res });
+    return res;
+  } catch (error) {
+    console.log("error? ", error);
+    throw error;
+  }
+}
+
+export async function getTimezoneTime(
+  path: string,
+  config?: ApiConfig
+): Promise<TimezoneTime> {
+  try {
+    const res = await fetch(`https://worldtimeapi.org/api/timezone/${path}`, {
+      signal: config?.controller?.signal,
+    }).then((r) => {
+      if (!r.ok) {
+        throw new Error(r.statusText);
+      }
+      return r.json() as Promise<TimezoneTime>;
+    });
+    console.log({ res });
+    return res;
+  } catch (error) {
+    console.log({ error });
+    throw error;
+  }
+}
