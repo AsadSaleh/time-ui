fe69fbe724beec7641b65c876c4679d77ed5ffb1
As'ad Saleh Umar <azzzad.s@gmail.com>
2023-11-16 11:16:32 +0700

    Style CityTimeCard

--- src/components/CityTimeCard.tsx
+++ src/components/CityTimeCard.tsx
@@ -42,20 +42,28 @@ export default function CityTimeCard(props: CityTimeCardProps) {
   // Format city name before display:
   const formattedCity = fromUnderscoreToPascal(city);
 
+  // Create text like "UTC+7" or "UTC-9":
+  const timezoneUtcOffsetText = `UTC${
+    offsetFromUtc >= 0 ? `+${offsetFromUtc}` : offsetFromUtc
+  }`;
+
   return (
-    <div className="group bg-stone-300 rounded-xl flex flex-col justify-center items-center py-6 relative bg-white/40 backdrop-blur">
-      <div
+    <div className="group bg-stone-300 rounded-xl flex px-4 flex-col justify-center items-center py-6 relative bg-white/40 backdrop-blur">
+      <button
+        type="button"
         onClick={() => props.onDelete?.()}
         className="bg-stone-100/40 hover:bg-stone-100 transition-all ease-in-out group-hover:block absolute hidden right-2 top-2 cursor-pointer p-1 rounded-lg"
       >
         <TrashIcon className="text-red-200" />
-      </div>
+      </button>
       <h4 className="text-3xl">{formattedCity}</h4>
-      <p className="text-lg italic">{props.label ?? "-"}</p>
+      <p className="text-lg italic">{props.label}</p>
 
       {clock && <TickingClock timezone={props.location} />}
 
-      <p className="text-center">Timezone: {abbreviation}</p>
+      <p className="text-center mt-1">
+        Timezone: {abbreviation} ({timezoneUtcOffsetText})
+      </p>
       <p className="text-center mt-4 text-black/60">{offsetText}</p>
     </div>
   );
@@ -69,7 +77,7 @@ function TickingClock({ timezone }: { timezone: string }) {
   const date = new Date();
 
   return (
-    <p className="text-2xl md:text-3xl lg:text-4xl xl:text-6xl mt-4 font-mono italic text-center">
+    <p className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-4 font-mono italic text-center">
       {date.toLocaleTimeString("en-US", {
         timeZone: timezone,
         hour12: true,
